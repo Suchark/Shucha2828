095f2c3535babc06cda6db3e971b39a41198ea36
Shuchanuch Rinkum <Suchanuch28@gmail.com>
2021-11-30 18:21:43 +0700

    lab 7.3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -13,3 +13,40 @@ int main(){
   cout << adiff(1000,280);
   cout << adiff(60,244);
 }
+
+
+int adiff(int A,int B)
+  {
+  
+    int C;
+  
+    while( A > 360 ){
+      A -= 360;
+    }
+    while( B > 360 ){
+      B -= 360;
+    }
+    if( A > B ){
+      C = A - B;
+    }else{
+      C = B - A;
+    }
+    while( C >= 360 ){
+      C -= 360;
+    }
+    if( C > 180 ){
+      C = 360 - C;
+    }
+  
+   return C ;
+  
+  }
+
+
+
+
+
+
+ 
+
+
